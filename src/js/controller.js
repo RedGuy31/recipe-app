e5fb98decdcd79f1fea456ec0cb254f8ecafa7f1
Irakli Maisuradze <md.dr31.19@gmail.com>
2022-03-11 18:05:49 -0800

    reset

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,31 +1,9 @@
-import * as model from "./model.js";
-import recipeView from "./views/recipeView.js";
+const recipeContainer = document.querySelector(".recipe");
 
-import "regenerator-runtime/runtime";
-
-const recipeContainer = document.querySelector(".Recipe");
-
-const controlRecipes = async function () {
-  try {
-    const id = window.location.hash.slice(1);
-
-    if (!id) return;
-    recipeView.renderSpiner();
-
-    // load recipe
-
-    await model.loadRecipe(id);
-    const { recipe } = model.state.recipe;
-
-    // render recipe
-    recipeView.render(model.state.recipe);
-  } catch (err) {
-    alert(err);
-  }
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
 };
-
-controlRecipes();
-
-["hashchange", "load"].forEach((ev) =>
-  window.addEventListener(ev, controlRecipes)
-);
